c895525f9ac28a3e8a30575a48a58c18943314a0
Pjotrs Osnovskis <pjotrs.osnovskis@gmail.com>
2022-12-11 14:56:07 +0200

    fix: finished index and footer mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-$('#navbar-toggler').on('click', ()=> {
+$('#navbar-toggler').on('click', () => {
     $('.overlay').css('margin-top', $('.navbar').outerHeight())
 
     if ($('#navbar-toggler').attr('aria-expanded') == 'true') {
@@ -10,9 +10,9 @@ $('#navbar-toggler').on('click', ()=> {
 
 var scrollPosition = [
     self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-  ];
-  var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
-  html.data('scroll-position', scrollPosition);
-  html.data('previous-overflow', html.css('overflow-x'));
-  html.css('overflow-x', 'hidden');
-  window.scrollTo(scrollPosition[0], scrollPosition[1]);
+];
+var html = jQuery('body'); // it would make more sense to apply this to body, but IE7 won't have that
+html.data('scroll-position', scrollPosition);
+html.data('previous-overflow', html.css('overflow'));
+html.css('overflow-x', 'hidden');
+window.scrollTo(scrollPosition[0], scrollPosition[1]);
