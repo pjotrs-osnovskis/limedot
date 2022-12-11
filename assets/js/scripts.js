36cb8a945bec2e608751a8b331647d8509e18c50
Pjotrs Osnovskis <pjotrs.osnovskis@gmail.com>
2022-12-11 10:45:15 +0200

    fix: positioning fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,12 @@ $('#navbar-toggler').on('click', ()=> {
         $('body').removeClass('fixed-position');
     }
 })
+
+var scrollPosition = [
+    self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+  ];
+  var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+  html.data('scroll-position', scrollPosition);
+  html.data('previous-overflow', html.css('overflow-x'));
+  html.css('overflow-x', 'hidden');
+  window.scrollTo(scrollPosition[0], scrollPosition[1]);
