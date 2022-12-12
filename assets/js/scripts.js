4790cb2ecc686888df8a667d762cfcc993b410dc
Pjotrs Osnovskis <pjotrs.osnovskis@gmail.com>
2022-12-12 21:22:55 +0200

    fix: fixed up index page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,7 @@ $('#navbar-toggler').on('click', () => {
 
     if ($('#navbar-toggler').attr('aria-expanded') == 'true') {
         $('body').addClass('fixed-position');
+
     } else {
         $('body').removeClass('fixed-position');
     }
