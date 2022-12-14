6ac9df3e2419ec9ed035ece87939db323ec3a017
Pjotrs Osnovskis <pjotrs.osnovskis@gmail.com>
2022-12-14 23:57:37 +0200

    fix: navbar blut fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,8 @@
 $('#navbar-toggler').on('click', () => {
-    $('.overlay').css('margin-top', $('.navbar').outerHeight())
+    
     if ($('#navbar-toggler').attr('aria-expanded') == 'true') {
         $('body').addClass('fixed-position');
     } else {
         $('body').removeClass('fixed-position');
     }
 })
-
-var scrollPosition = [
-    self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-];
-var html = jQuery('body'); // it would make more sense to apply this to body, but IE7 won't have that
-html.data('scroll-position', scrollPosition);
-html.data('previous-overflow', html.css('overflow'));
-html.css('overflow-x', 'clip');
-window.scrollTo(scrollPosition[0], scrollPosition[1]);
