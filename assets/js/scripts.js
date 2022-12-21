2c406744e61d377dca9656156bfe3dd74e4bfd5f
Pjotrs Osnovskis <pjotrs.osnovskis@gmail.com>
2022-12-21 15:13:58 +0200

    fix: register update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,3 +5,22 @@ $('#navbar-toggler').on('click', () => {
         $('body').removeClass('fixed-position');
     }
 })
+
+$('#notPEPCheckbox').on('change', (e)=>{
+    e.preventDefault();
+    if ($('#notPEPCheckbox').is(':checked')){
+        $('.nextStepButton').attr('disabled', false);
+    } else {
+        $('.nextStepButton').attr('disabled', true);
+    }
+})
+
+$('#taxCountryCheckbox').on('change', (e)=>{
+    e.preventDefault();
+    if ($('#taxCountryCheckbox').is(':checked')){
+        $('#taxCountryPicker').show();
+    } else {
+        $('#taxCountryPicker').hide();
+    }
+
+})
